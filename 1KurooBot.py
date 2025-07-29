f15a4d6e01aadc1dc25d03ffc31b6a46d2d54660
KuroM0M0 <m0m0_lp@web.de>
2025-07-29 18:10:31 +0200

    Command Profil added

--- 1KurooBot.py
+++ 1KurooBot.py
@@ -235,7 +235,7 @@ async def stats(interaction: discord.Interaction, person: discord.Member = None)
             await interaction.followup.send(f"{targetName} hat seine Stats versteckt.", ephemeral=True)
             return
         else:
-            StatsTarget(person)
+            await StatsTarget(person)
             await interaction.delete_original_response()
             await channel.send(embed=embedTarget)
             return
@@ -457,6 +457,56 @@ async def sparkDisable(interaction: discord.Interaction):
     else:
         await interaction.response.send_message("Dieser Befehl ist nur für Premium Nutzer verfügbar.", ephemeral=True)
 
+
+@bot.tree.command(name="profil", description="Zeige dein Profil an")
+async def profil(interaction: discord.Interaction, user: discord.User = None):
+    if user is None:
+        user = interaction.user
+    
+    userID = user.id
+    userName = user.display_name
+    sparkCount = getSparkCount(connection, userID)
+    Premium = getPremium(connection, userID)
+    PremiumTimestamp = getPremiumTimestamp(connection, userID)
+    privacy = getProfilPrivateSetting(connection, userID)
+
+    if privacy == True:
+        if userID != interaction.user.id:
+            await interaction.response.send_message("Diese Person hat ihr Profil auf Privat.", ephemeral=True)
+            return
+
+
+    embed = discord.Embed(
+        title=f"Profil von {userName}",
+        color=0x005b96
+    )
+    embed.set_thumbnail(url=user.display_avatar.url)
+    embed.add_field(name="🗓️Beigetreten am", value=user.joined_at.strftime("%d.%m.%Y"), inline=True)
+
+    if getBirthday(connection, userID) is not None:
+        embed.add_field(name="🎂Geburtstag", value=getBirthday(connection, userID), inline=True)
+
+    if Premium == True:
+        dt = datetime.fromisoformat(PremiumTimestamp)
+        unix_timestamp = int(dt.timestamp())
+        embed.add_field(name="Premium seit", value=f"<t:{unix_timestamp}:f>", inline=False)
+
+
+    embed.add_field(name="\u200b", value="\u200b", inline=False) #leerzeile
+
+    embed.add_field(
+    name="📨 Versendete Sparks",
+    value=f"{sparkCount} Sparks",
+    inline=True
+    )
+    await interaction.response.send_message(embed=embed)
+
+
+@bot.tree.command(name="reveal", description="Lasse dir anzeigen von wem ein Spark gesendet wurde!")
+async def reveal(interaction: discord.Interaction, SparkID: int):
+    await interaction.response.defer(ephemeral=True)
+
+    await interaction.followup.send("")
 
 
         
