15d22150605728429408a330728747269768f89d
KuroM0M0 <m0m0_lp@web.de>
2025-10-29 12:44:27 +0100

    Test

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-#BotToken = os.getenv("BotToken")
-BotToken = os.getenv("TestBotToken") #Testbot
-
-NSFWRoleID = 1329134672382398464#1186568822879170600 das hintere sind die echten Werte
-ServerID = 475295112453423125#1185618335950438500 das davor nur zum Testen
-
-ChannelID = 1329134639574421575#1426953845782876170 hintere ist richtiger Wert
